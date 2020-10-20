3e8d4107fdad1c52ebd3298cbaeb20ac97327786
Carrie Valencia <carrie.valencia@phoenix.edu>
2020-10-20 02:06:50 +0000

    Added s3 bucket module take 2

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
-module "s3-bucket" {
-  #source  = "app.terraform.io/CarrieV-Training/s3-bucket/aws"
-  source  = "terraform-aws-modules/s3-bucket/aws"
+module "s3-bucket" "s3Bucket"{
+  source  = "app.terraform.io/CarrieV-Training/s3-bucket/aws"  
   version = "1.15.0"
   # insert required variables here
   bucket_prefix = "Carrie-Valencia"
+  region = "us-east-1"
 }
