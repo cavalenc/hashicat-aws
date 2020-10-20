907e76c9688ab21090a19ef70904bc5ca1dd0f38
Carrie Valencia <carrie.valencia@phoenix.edu>
2020-10-20 02:18:50 +0000

    Added s3 bucket module take 3

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,4 +1,5 @@
-module "s3-bucket-cv" {
+module "s3-bucket" {
+  bucket = "s3-bucket-tb"
   source  = "app.terraform.io/CarrieV-Training/s3-bucket/aws"  
   version = "1.15.0"
   # insert required variables here
