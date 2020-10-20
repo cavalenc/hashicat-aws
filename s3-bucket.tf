0fe8335a26808c126f44eb7d5f70ac9b9c251ed5
Carrie Valencia <carrie.valencia@phoenix.edu>
2020-10-20 02:34:19 +0000

    Added s3 bucket module take 6

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,6 @@
 module "s3-bucket" {
-  bucket = "Carrie-Valencia-s3-bucket"
-  source  = "app.terraform.io/CarrieV-Training/s3-bucket/aws"  
+  source  = "app.terraform.io/CarrieV-Training/s3-bucket/aws"
   version = "1.15.0"
   # insert required variables here
-  #bucket_prefix = "Carrie-Valencia"
-  create_bucket = true
+  bucket_prefix = "Carrie-Valencia"
 }
